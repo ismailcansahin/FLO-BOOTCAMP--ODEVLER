708483e39c1db9445df39188e81263ed6b87d30d
İsmail Can ŞAHİN <109991366+ismailcansahin@users.noreply.github.com>
2022-11-11 15:47:23 +0300

    Update index.php

--- ODEV-2/index.php
+++ ODEV-2/index.php
@@ -17,7 +17,6 @@
 
     ?>
 
-
     <form action="" method="post">
         <table border='1' width='100%'>
         <thead>
@@ -95,112 +94,21 @@
         echo "</tr>";
     }
     
-
-
-
-
     echo "</table>";
-
-    
-
-
-    
-
-
-
-    // if(empty($_SESSION['urun1'])){
-    //     echo "Kinetix ARMOR Ürünü Sepete ".$_SESSION['urun1']." Adet Sepete Eklenmiştir.";
-    // }
-
-    // echo "<br>";
-
-    // if(empty($_SESSION['urun2'])){
-    //     echo "Adidas AESTHETO ".$_SESSION['urun2']." Adet Sepete Eklenmiştir.";
-    // }
-
-
-
-    
-
-    // $sayac = 0;
-    // foreach($_SESSION as $product){
-
-    //     foreach($product as $key => $value){
-    //         if($key == 0){
-    //             echo "<td>".$value."</td>";
-    //             echo "SIRA1";
-    //         }else if($key == 1){
-    //             echo "<td>".$value."</td>";
-    //             echo "SIRA 2";
-    //             $p = $value;
-    //         }else if($key == 2){
-    //             echo "SIRA 3</td>";
-    //             $q = $value;
-    //         }
-    //     }
-    // }
-
-
-    // $_SESSION['urun0']="$marka0";
-    // $_SESSION['urun1']="$marka1";
-    // $_SESSION['urun2']="$marka2";
-   
-
-
-
-
-
-
-
-    
-
-  
-
-    // echo "<table border='1' width='100%'>
-    // <tr>
-    // <td>Ürün Adı</td>
-    // <td>Adet </td>
-    // <td>Toplam</td>
-    // </tr>";
-
-    // foreach($_SESSION['sepet'] as $val){
-    //     $sayac++;
-    //     print_r($val)
-    // }
-    
-    
-    // echo "</table>";    
-
 
 
 ?>
-<br><br><br><br>
+<br><br>
 <form action="" method="post">
-
 <input style= "text-align:right" type="submit" value="Sepeti Sıfırla" name=sifirla>
-
 </form>
-
-
 <?php
-
 
 
 if($_POST["sifirla"] == TRUE){
     session_destroy($_SESSION);
     }
 ?>
-
-
-
-<!-- Ürünleri bir dizide tutacağınız için,
-dizinin indisini kullanarak her bir adet inputuna
-farklı bir isim verebilirsiniz.
-Mesela urun0, urun1, urun2 gibi.
-Buradaki 0-1-2 dizinin indisi.
-Session’a da bu indisle kaydedip
-sonra diziden bu indisteki ürünü çağırıp
-tekrar sepeti listelersiniz. -->
 
 
 </body>
