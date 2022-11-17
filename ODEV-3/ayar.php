99bbdfc2a6d14274b7a546ab14522c68ddf95cd1
İsmail Can ŞAHİN <109991366+ismailcansahin@users.noreply.github.com>
2022-11-17 18:36:45 +0300

    Update ayar.php

--- ODEV-3/ayar.php
+++ ODEV-3/ayar.php
@@ -5,5 +5,4 @@ $mysqlsifre = "1234";
 
 $baglan = new PDO("mysql:host=$mysqlsunucu;dbname=telefonrehberi;charset=utf8", $mysqlkullanici, $mysqlsifre);
 $baglan->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 ?>
